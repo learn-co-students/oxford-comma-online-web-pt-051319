85d292764116946a5b7c81edc422a457c27a9e3f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-14 18:43:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length ==1
+    str =array[0]
+   #return str
+  elsif array.length ==2
+    str =array[0]<<" and "<< array[1]
+  else
+    str =array.fetch(-1)
+    str2 = "and"<<" #{str}"
+    array.pop
+    array<<str2
+    array.join(", ")
+  end
 end
+ 
