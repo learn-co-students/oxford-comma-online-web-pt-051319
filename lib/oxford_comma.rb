56dc566fde0099be6c49395ef366dfe2b5709ac2
Aida Abdikulova <abdikulova@gmail.com>
2019-05-22 03:48:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    then return array.join
+  elsif array.size == 2
+    then return array.join(" and ")
+  else
+    array[0,array.size-1].push("and #{array[-1]}").join(", ")
 end
+  end
